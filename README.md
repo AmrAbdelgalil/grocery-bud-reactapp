c34b2df5b86664dd1fe782465124d7b81ace4620
AmrAbdelgalil <93317873+AmrAbdelgalil@users.noreply.github.com>
2022-05-25 08:20:18 +0200

    Complete Project and add Readme File

---
+++ README.md
@@ -0,0 +1,12 @@
+# Grocery Bud
+
+### Description:
+
+        -Buliding Grocery Bud App with Local Storage.
+        -You can add, edit or delete item. Also you can Clear All list.
+
+### What I learn?
+
+        -Using of Window Local Storage Object.
+        -Hooks: `useState()`, `useEffect() `
+        -Using of React icons liberary.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,117 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
-function App() {
-  return <h2>grocery bud setup</h2>
+//function to get Local storage
+function getLocalStorage() {
+  const list = localStorage.getItem("list");
+
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
 }
 
-export default App
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  const [alert, setAlert] = useState({
+    type: "",
+    show: false,
+    msg: "",
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+
+  // Local Storage
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      //if no value entered.
+      showAlert(true, "danger", "please insert item");
+    } else if (name && isEditing) {
+      //deal with edit item
+      //to check first if there is value or not.
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      showAlert(true, "success", "item changed");
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+    } else {
+      showAlert(true, "success", "item added to the list");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(
+      list.filter((item) => {
+        return item.id !== id;
+      })
+    );
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => {
+      return item.id === id;
+    });
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} list={list} removeAlert={showAlert} />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "Edit" : "Submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear all
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default App;
